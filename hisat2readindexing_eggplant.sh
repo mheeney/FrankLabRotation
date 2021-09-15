cde5e86fa596b9f9a6d2ab01e042c09fe38c618b
Michelle <63256511+mheeney@users.noreply.github.com>
2021-09-15 13:56:58 -0400

    using hisat2 index reads against eggplant genome

---
+++ hisat2readindexing_eggplant.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+# Before running HISAT2 you need to add it to you PATH
+source /programs/HISAT2/hisat2.sh
+
+# Samtools' latest version is already in BioHPC Path
+
+#setup for-loop
+dir=$1
+    cd $dir
+    file=`ls *.gz | sed 's/_trimmed.fq.gz//g' | awk '{print $1}' | sort | uniq`
+
+    for i in $file 
+        do
+        echo $i
+
+#Run HISAT2
+hisat2 -x /local/workdir/michellefranklab/eggplantindex/eggplantindex \
+        -U ${dir}/${i}_trimmed.fq.gz \
+        --met-file ${dir}/${i}.err \
+        --summary-file ${dir}/${i}.out \
+        --no-unal \
+        -p 8 | samtools view -Sb | samtools sort -o ${dir}/${i}.bam -@ 8 &&
+        # ^Use Samtools to make .bam .sam files^        
+        
+# Use Samtools to index the genome
+samtools index -@ 90 ${dir}/${i}.bam
+
+done
